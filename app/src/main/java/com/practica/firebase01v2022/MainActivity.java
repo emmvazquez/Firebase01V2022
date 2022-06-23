3d2b15e8a9d7d82726e61aa5b363a35755d8b200
emmanuel <emmvazquez@gmail.com>
2022-06-23 13:48:12 -0500

    Configurando firebase

---
+++ app/src/main/java/com/practica/firebase01v2022/MainActivity.java
@@ -0,0 +1,40 @@
+package com.practica.firebase01v2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+EditText editText;
+Button button;
+TextView textView;
+EditText editTextColeccion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editTextColeccion = findViewById(R.id.editTextColeccion);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference reference = database.getReference(editTextColeccion.getText().toString());
+                reference.setValue(editText.getText().toString());
+            }
+        });
+
+    }
+}
